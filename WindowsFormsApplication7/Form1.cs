77a1d624646d67fe7c8b7d3eb8c35fe915dc2b99
losineff <92970535+losineff@users.noreply.github.com>
2021-10-24 22:46:14 +0300

    a

---
+++ WindowsFormsApplication7/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int a = Convert.ToInt32(textBox1.Text);
+            label1.Text =a.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int a = Convert.ToInt32(textBox1.Text);
+            a++;
+            label1.Text = a.ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int a = Convert.ToInt32(textBox1.Text);
+            a--;
+            label1.Text=a.ToString();
+        }
+    }
+}
